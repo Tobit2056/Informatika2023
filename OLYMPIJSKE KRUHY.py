08d459582c0d24cf657f68383dea68109e3f98b8
Tobit2056 <144805761+Tobit2056@users.noreply.github.com>
2023-11-22 10:55:26 +0100

    Update OLYMPIJSKE KRUHY.py

--- OLYMPIJSKE KRUHY.py
+++ OLYMPIJSKE KRUHY.py
@@ -6,11 +6,12 @@ class OlympicRings(Scene):
     def construct(self):
         self.camera.background_color = WHITE
         # Barvy pro olympijské kruhy
+        width = 20
         colors = [BLACK, BLUE, RED, YELLOW, GREEN]
         radius = 1  # Společný poloměr pro všechny kruhy
 
         # Vytvoření olympijských kruhů s stejným poloměrem
-        rings = [Circle(radius=radius, color=color) for color in colors]
+        rings = [Circle(radius=radius, color=color, stroke_width=width) for color in colors]
 
         # Nastavení pozic pro kruhy
         positions = [(0, 0, 0), (-2.5, 0, 0), (2.5, 0, 0), (-1.25, -1, 0), (1.25, -1, 0)]

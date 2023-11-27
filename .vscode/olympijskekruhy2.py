6bc6de54abc5d33ae39db4b63dcb9eaef5b87533
Tobit2056 <144805761+Tobit2056@users.noreply.github.com>
2023-11-27 14:02:16 +0100

    Update olympijskekruhy2.py
    Olympijské kruhy pomocí arc

--- .vscode/olympijskekruhy2.py
+++ .vscode/olympijskekruhy2.py
@@ -1,26 +1,29 @@
 %%manim -v WARNING -qm OlympicRings
-​
+
 from manim import *
-​
+
 class OlympicRings(Scene):
     def construct(self):
         self.camera.background_color = WHITE
         # Barvy pro olympijské kruhy
-        colors = [BLACK, BLUE, RED, YELLOW, GREEN]
+        colors = [BLACK, BLACK, BLUE, RED, YELLOW, YELLOW, GREEN, GREEN]
         radius = 1  # Společný poloměr pro všechny kruhy
         width = 20
-        start_angles = [0, 45, 90, 135, 180]  # Začáteční úhel oblouku v stupních
-        end_angle = 360  # Koncový úhel oblouku v stupních
-​
+
         # Vytvoření olympijských kruhů s pomocí Arc
-        arcs = [Arc(radius=radius, start_angle=start_angle * DEGREES, angle=end_angle * DEGREES, color=color, stroke_width=width) for start_angle, color in zip(start_angles, colors)]
-​
+        arcs = []
+        angles = [295, 190, 295, 192, 115, 15, 115, 15]  # Angles for each ring
+        end_angle = [235, 85 ,340 ,340 ,235 , 80, 235, 80 ]
+        for i in range(len(colors)):
+            arc = Arc(radius=radius, start_angle=angles[i] * DEGREES, angle=end_angle[i] * DEGREES, color=colors[i], stroke_width=width)
+            arcs.append(arc)
+
         # Nastavení pozic pro kruhy
-        positions = [(0, 0, 0), (-2.5, 0, 0), (2.5, 0, 0), (-1.25, -1, 0), (1.25, -1, 0)]
-​
+        positions = [(0, 0, 0), (-0.45, -0.615, 0), (-2.5, 0, 0), (2.5, 0, 0), (-1.25, -1, 0), (-0.8, -0.38, 0), (1.25, -1, 0), (1.7, -0.38, 0)]
+
         # Animace pro vytvoření a zobrazení olympijských kruhů
-        for arc, position in zip(arcs, positions):
-            arc.move_to(position)
-            self.play(Create(arc))
-​
+        for i in range(len(arcs)):
+            arcs[i].move_to(positions[i])
+            self.play(Create(arcs[i]))
+
         self.wait(1)  # Počkejte jednu sekundu na konci animace
